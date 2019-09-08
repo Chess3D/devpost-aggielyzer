4b2549b8f3a61d70e3918aca0329dd04f17c0a12
Bennett <bennettacarley@gmail.com>
2019-09-07 21:47:51 -0500

    It's broken

--- DevpostAnalyzer.py
+++ DevpostAnalyzer.py
@@ -1,2 +1,16 @@
-import scrapy
+import scrapy, requests
+from bs4 import BeautifulSoup
 
+# 
+website = 'https://devpost.com/software/search?page='
+
+# Get the max page count
+def get_page_count()
+
+
+lastPage = 
+
+for link in devpost.find_all('page='):
+    print(link)
+
+print(devpost)
