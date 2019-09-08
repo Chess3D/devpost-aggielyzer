1d94b96cda87a2da8bef94d9045f269276c6e69f
Bennett <bennettacarley@gmail.com>
2019-09-07 23:24:45 -0500

    Reformatted code to be more readable

--- DevpostAnalyzer.py
+++ DevpostAnalyzer.py
@@ -7,6 +7,10 @@ website = 'https://devpost.com/software/search?page='
 # Keyword looking to be found
 keyword = "Oh no! Looks like there's no software matching your query."
 
+# The last page with useful text
+lastPage = binary_search()
+
+
 # Checks if the keyword is on the desired page
 def content_check(page):
     html = urllib.request.urlopen(website + str(page)).read()
@@ -18,40 +22,22 @@ def content_check(page):
 
 # Preforms a binary search to find the last page
 def binary_search():    
-    i = 5000
-    currentPage = 1
-    pastPage = currentPage + 1
+    i = 10000
+    currentPage = 0
+    pastPage = i
     state = False
 
     while True:
-
-        while content_check(currentPage) == state:
-            print('Current Page:',currentPage, '\tPast Page:', pastPage, '\tState:', state)
-            
-            if currentPage == pastPage:
-                return currentPage
-            elif state == True:
-                pastPage = currentPage
-                currentPage -= i
-            else:
-                pastPage = currentPage
-                currentPage += i
-
         state = bool((int(state) + 1) % 2)
         i = i // 2
 
-    return currentPage
+        while content_check(currentPage) == state:        
+            if currentPage == pastPage:
+                return currentPage
+            else:
+                pastPage = currentPage
 
-
-print(binary_search())
-
-# Get the max page count
-# def get_page_count()
-#     for 
-
-# lastPage = 
-
-# for link in devpost.find_all('page='):
-#     print(link)
-
-# print(devpost)
+            if state == True:
+                currentPage -= i
+            else:
+                currentPage += i
